3a9f5c367c8157f2ed8953f79ef59292e774a852
Antoine Lefevre <antoine.lefevre7@etu.univ-lorraine.fr>
2021-02-05 16:59:19 +0100

    Déplacement du projet

---
+++ Assets/Scripts/Caméra/ZoomDezoom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomDezoom : MonoBehaviour
+{
+    public KeyCode zoom;
+    public KeyCode dezoom;
+
+    void Update()
+    {
+        if(Input.GetKey(zoom) && Camera.main.fieldOfView > 65)
+         {
+             Camera.main.fieldOfView -= 1;
+         }
+
+         if (Input.GetKey(dezoom) && Camera.main.fieldOfView < 135)
+         {
+             Camera.main.fieldOfView += 1;
+         }
+    }
+}
