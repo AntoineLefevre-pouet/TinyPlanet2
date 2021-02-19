dd677bf986010867545222a11f6e9637371ab1a5
Antoine Lefevre <antoine.lefevre7@etu.univ-lorraine.fr>
2021-02-19 14:28:00 +0100

    Rotation de la sphere

---
+++ Assets/Scripts/Caméra/SphereRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void OnMouseDrag()
+    {
+        {
+            float rotX = Input.GetAxis("Mouse X")*rotationSpeed*Mathf.Deg2Rad;
+            float rotY = Input.GetAxis("Mouse Y") * rotationSpeed * Mathf.Deg2Rad;
+
+            transform.RotateAround(Vector3.up, -rotX);
+            transform.RotateAround(Vector3.right, rotY);
+        }
+    }
+}
