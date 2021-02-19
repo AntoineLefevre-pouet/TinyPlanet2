18ef08ce76f83513ba3b6b0a71b1bb149d46153f
Leili <leili.mirkhosravi@gmail.com>
2021-02-19 14:43:22 +0100

    Gestion de la UI de question
    création d'un game manager qui contient les fonction d'affichage de l'UI de question

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public GameObject questionUI;
+
+  public static GameManager instance;
+
+  private void Awake()
+  {
+    if(instance != null)
+    {
+      Debug.LogWarning("Il y a plus d'une instance de GameManager dans la scène");
+      return;
+    }
+
+    instance = this; // permet d'accéder au script depuis n'importe où
+  }
+
+  public void ShowQuestionUI()
+  {
+    if(questionUI.activeSelf == false) {
+      questionUI.SetActive(true);
+    }
+  }
+
+  public void HideQuestionUI()
+  {
+    if(questionUI.activeSelf == true) {
+      questionUI.SetActive(false);
+    }
+  }
+
+}
