85af5532ef0b1329d953ffee5f702386baa40102
Antoine Lefevre <antoine.lefevre7@etu.univ-lorraine.fr>
2021-02-19 15:47:41 +0100

    Arbres + amélioration du zoom

--- Assets/Scripts/Caméra/ZoomDezoom.cs
+++ Assets/Scripts/Caméra/ZoomDezoom.cs
@@ -7,16 +7,18 @@ public class ZoomDezoom : MonoBehaviour
     public KeyCode zoom;
     public KeyCode dezoom;
 
+    public float zoomSpeed;
+
     void Update()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.fieldOfView > 65)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > 5)
         {
-             Camera.main.fieldOfView -= 1;
+             Camera.main.orthographicSize -= zoomSpeed;
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.fieldOfView < 135)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < 50)
         {
-             Camera.main.fieldOfView += 1;
+             Camera.main.orthographicSize += zoomSpeed;
         }
     }
 }
